04436599421c9e011d8a9fe43e4bf7bff43b72a9
Vadim Smagin <magenvadim@gmail.com>
2024-05-19 04:02:23 +0300

    #3 Components has added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Header } from "./components";
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages";
+import { Home, FullPost, Registration, AddPost, Login } from "./pages";
 import Container from "@mui/material/Container";
 
 function App() {
@@ -11,6 +11,11 @@ function App() {
     <Container maxWidth="lg">
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/posts/:id' element={<FullPost/>}/>
+        <Route path='/add-post' element={<AddPost/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Registration/>}/>
+
       </Routes>
     </Container>
 
