d889fee8b87c5b7998a481519481f6faaefa9a2f
Vadim Smagin <magenvadim@gmail.com>
2024-05-19 03:55:19 +0300

    #2 Home page has added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Header } from "./components";
 import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages";
+import Container from "@mui/material/Container";
 
 function App() {
   return (
    <>
     <Header />
+
+    <Container maxWidth="lg">
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </Container>
+
    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
+import CssBaseline from "@mui/material/CssBaseline";
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from "@mui/material";
 import { BrowserRouter } from 'react-router-dom';
+import { theme } from "./theme";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+    
+    </BrowserRouter>
   </React.StrictMode>
 );
 
